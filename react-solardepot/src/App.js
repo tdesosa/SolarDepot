a072e9faaada869e8bd1fca0b3b1cbbc766bf8c7
tdesosa <tdesosa@gmail.com>
2018-11-27 17:04:49 -0700

    initial setup

---
+++ react-solardepot/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Solar Depot (Show Me Da Moneeee Meng)</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ solardepotProj/solardepotApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SolardepotappConfig(AppConfig):
+    name = 'solardepotApp'
