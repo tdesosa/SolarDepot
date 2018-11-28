cb4dcf7bd1d31bec1e822034e4e032b32872e0b2
tdesosa <tdesosa@gmail.com>
2018-11-27 22:49:37 -0700

    stripe form test successful

--- react-solardepot/src/App.js
+++ react-solardepot/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Elements, StripeProvider} from 'react-stripe-elements';
+import CheckoutForm from './CheckoutForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Solar Depot (Show Me Da Moneeee Meng)</h1>
+        <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+        <div className="example">
+          <h1>Stripe Docs</h1>
+          <Elements>
+            <CheckoutForm />
+          </Elements>
+        </div>
+      </StripeProvider>
       </div>
     );
   }
