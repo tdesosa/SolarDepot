dde67d426e624bfdb96994e887315be5c7f62449
tdesosa <tdesosa@gmail.com>
2018-11-28 13:15:27 -0700

    working on django and stripe implementation

--- solardepotProj/solardepotProj/settings.py
+++ solardepotProj/solardepotProj/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'solardepotApp'
+    'solardepotApp',
+    'payments',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'solardepotProj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
